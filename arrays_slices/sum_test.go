d880d4d152aeea2c4155b09263067f24666d51fc
tammarut <tammarut.nua@gmail.com>
2020-01-15 00:56:28 +0700

    remove unnecessary test case

--- arrays_slices/sum_test.go
+++ arrays_slices/sum_test.go
@@ -3,18 +3,6 @@ package arrayslice
 import "testing"
 
 func TestSum(t *testing.T) {
-	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
-
-		got := Sum(numbers)
-		want := 15
-
-		if got != want {
-			t.Errorf("got %d, want %d by given %v", got, want, numbers)
-		}
-	})
-	
-	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 
 		got := Sum(numbers)
@@ -23,5 +11,4 @@ func TestSum(t *testing.T) {
 		if got != want {
 			t.Errorf("got %d, want %d by given %v", got, want, numbers)
 		}
-	})
 }
