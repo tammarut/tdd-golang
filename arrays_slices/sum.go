50239d2c0f8a8874ddbf6c4f2a28f7c0ee573b29
tammarut <tammarut.nua@gmail.com>
2020-01-16 23:34:42 +0700

    append 0 when slice is empty

--- arrays_slices/sum.go
+++ arrays_slices/sum.go
@@ -17,8 +17,12 @@ func SumAll(numbersToSum ...[]int) (result []int) {
 
 func SumAllTails(sliceToSum ...[]int) (result []int) {
 	for _, numbers := range sliceToSum {
+		if len(numbers) == 0 {
+			result = append(result, 0)
+		} else {
 		tail := numbers[1:]
 		result = append(result, Sum(tail))
+		}
 	}
 	return result	
 }
