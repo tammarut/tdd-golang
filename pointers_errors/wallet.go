c0f95044a1230ba04a9cd59a2fcfb10e22135be8
tammarut <tammarut.nua@gmail.com>
2020-01-31 01:15:20 +0700

    change to pointer wallet

--- pointers_errors/wallet.go
+++ pointers_errors/wallet.go
@@ -1,13 +1,16 @@
 package wallet
 
+import "fmt"
+
 type Wallet struct {
 	balance int
 }
 
-func (w Wallet) Deposit(coin int) {
-	w.balance = coin
+func (w *Wallet) Deposit(coin int) {
+	w.balance += coin
+	fmt.Printf("address of balance in Deposit is %v\n", &w.balance)
 }
 
-func (w Wallet) Balance() int {
-	return 10
+func (w *Wallet) Balance() int {
+	return w.balance
 }
--- pointers_errors/wallet_test.go
+++ pointers_errors/wallet_test.go
@@ -1,12 +1,16 @@
 package wallet
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 	wallet.Deposit(10)
 
 	got := wallet.Balance()
+	fmt.Printf("address of balance in test is %v\n", &wallet.balance)
 	want := 10
 
 	if got != want {
