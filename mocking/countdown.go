750e89e1e020aafdca65854c58f28e135d4204d1
tammarut <tammarut.nua@gmail.com>
2020-02-01 03:03:05 +0700

    Test configurableSleeper and SpyTime

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -38,6 +38,23 @@ func (s *CountdownOperationSpy) Write(p []byte) (n int, err error) {
 	return
 }
 
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
 const write = "write"
 const sleep = "sleep"
 const finalWord = "Go!"
--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -6,6 +6,8 @@ import "bytes"
 
 import "reflect"
 
+import "time"
+
 func TestCountdown(t *testing.T) {
 	t.Run("prints 3 to Go!", func(t *testing.T) {
 		buffer := &bytes.Buffer{}
@@ -41,4 +43,18 @@ Go!`
 			t.Errorf("wanted calls %v but got %v", want, spySleepPrinter.Calls)
 		}
 	})
+}
+
+func TestConfigurableSleep(t *testing.T) {
+	sleepTime := 5 * time.Second
+	spyTime := &SpyTime{}
+	config := ConfigurableSleeper{
+		sleepTime,
+		spyTime.Sleep,
+	}
+	config.Sleep()
+
+	if sleepTime != spyTime.durationSlept {
+		t.Errorf("shuold have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
+	}
 }
