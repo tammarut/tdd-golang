70d6986f4045a72323f675a202d8cb67e30f8698
tammarut <tammarut.nua@gmail.com>
2020-01-31 16:38:49 +0700

    Unknown word search case

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,13 @@
 package maps
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (dic Dictionary) Search(word string) string {
-	return dic[word]
+func (dic Dictionary) Search(word string) (string, error) {
+	defination, ok := dic[word]
+	if !ok {
+		return "", errors.New("couldn't find the word you were looking for")
+	}
+	return defination, nil
 }
