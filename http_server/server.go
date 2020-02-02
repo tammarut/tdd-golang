8741140f1f659327ab4beeba5b87382617e0da78
tammarut <tammarut.nua@gmail.com>
2020-02-02 23:51:52 +0700

    Refactor, DRY up trim url

--- http_server/server.go
+++ http_server/server.go
@@ -20,11 +20,13 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
 	switch r.Method {
 	case http.MethodPost:
-		p.processWin(w, r)
+		p.processWin(w, r, player)
 	case http.MethodGet:
-		p.showScore(w, r)
+		p.showScore(w, r, player)
 	}
 }
 
@@ -36,15 +38,12 @@ func (s *StubPlayerStore) RecordWin(name string) {
 	s.winCalls = append(s.winCalls, name)
 }
 
-func (p *PlayerServer) processWin(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
+func (p *PlayerServer) processWin(w http.ResponseWriter, r *http.Request, player string) {
 	p.store.RecordWin(player)
 	w.WriteHeader(http.StatusAccepted)
 }
 
-func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
-
+func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request, player string) {
 	scores := p.store.GetPlayerScore(player)
 	if scores == 0 {
 		w.WriteHeader(http.StatusNotFound)
