35707866ee742cab859e8eb7d5e39101dd85085c
tammarut <tammarut.nua@gmail.com>
2020-02-02 22:34:44 +0700

    Pass in a PlayerStore in main

--- http_server/main.go
+++ http_server/main.go
@@ -5,8 +5,15 @@ import (
 	"net/http"
 )
 
+type SaberScores struct{}
+
+func (s *SaberScores) GetPlayerScore(name string) int {
+	return 100
+}
+
 func main() {
-	server := &PlayerServer{}
+	saber := SaberScores{}
+	server := &PlayerServer{&saber}
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000, %v", err)
 	}
