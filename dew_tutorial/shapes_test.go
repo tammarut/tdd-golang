e79943568f386505915fa37cb0b0af857210ed76
tammarut <tammarut.nua@gmail.com>
2020-02-25 20:16:50 +0700

    Refactor each calculate area to once test function

--- dew_tutorial/shapes_test.go
+++ dew_tutorial/shapes_test.go
@@ -2,33 +2,29 @@ package tutorial
 
 import "testing"
 
-func TestSquare(t *testing.T) {
-	shape := Square{side: 8}
-	expected := 64.0
-
-	assertCheckArea(t, expected, shape)
-}
-
-func TestTriangle(t *testing.T) {
-	shape := Triangle{2.0, 5.0}
-	expected := 5.0
-
-	assertCheckArea(t, expected, shape)
-}
-
-func TestCircle(t *testing.T) {
-	ball := Circle{7.0}
-	expected := 147.0
-
-	assertCheckArea(t, expected, ball)
-
-}
-
 func assertCheckArea(t *testing.T, expected float64, shape Shape) {
 	t.Helper()
 	actual := shape.CalculateArea()
 
 	if expected != actual {
 		t.Errorf("expected %.2f but actual %.2f", expected, actual)
+	}
+}
+
+func TestCalculateArea(t *testing.T) {
+	areaTest := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Triangle", shape: Triangle{2.0, 5.0}, hasArea: 5.0},
+		{name: "Square", shape: Square{8.0}, hasArea: 64.0},
+		{name: "Circle", shape: Circle{7.0}, hasArea: 147.0},
+	}
+
+	for _, v := range areaTest {
+		t.Run(v.name, func(t *testing.T) {
+			assertCheckArea(t, v.hasArea, v.shape)
+		})
 	}
 }
